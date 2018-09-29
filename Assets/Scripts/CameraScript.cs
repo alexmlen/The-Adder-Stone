bfdf2303b7d4ea6aa609b12acc54a74b318658ba
Alexander Len <lenalexm@gmail.com>
2018-09-28 23:23:56 -0700

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+  private Transform player;
+  [SerializeField]
+  private float panSpeed;
+  [SerializeField]
+  private float height;
+  [SerializeField]
+  private float distance;
+	void Start () {
+    player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var newPosition = new Vector3(player.position.x, player.position.y + height, player.position.z - distance);
+    transform.position = Vector3.Lerp(transform.position, newPosition, panSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+  [SerializeField]
+	private float movespeed;
+  private Animator anim;
+  private Rigidbody rb;
+  private bool moving;
+  private float xaxis, zaxis, lastx, lastz;
+	void Start () {
+		anim = GetComponent<Animator>();
+    rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    xaxis = Input.GetAxisRaw("Horizontal");
+    zaxis = Input.GetAxisRaw("Vertical");
+    moving = (xaxis != 0 || zaxis != 0);
+    anim.SetFloat("XAxis", xaxis);
+    anim.SetFloat("ZAxis", zaxis);
+    anim.SetBool("Moving", moving);
+
+    if(moving == true){
+      anim.SetFloat("LastX", xaxis);
+      lastx = xaxis;
+      anim.SetFloat("LastZ", zaxis);
+      lastz = zaxis;
+    }
+
+    if(lastx == -1){
+      GetComponent<SpriteRenderer>().flipX = true;
+    } else {
+      GetComponent<SpriteRenderer>().flipX = false;
+    }
+
+    rb.AddForce(movespeed * Time.deltaTime * zaxis * transform.forward, ForceMode.VelocityChange);
+    rb.AddForce(movespeed * Time.deltaTime * xaxis * transform.right, ForceMode.VelocityChange);
+	}
+}
