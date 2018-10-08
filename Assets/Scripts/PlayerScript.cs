e0546824a54eee42da946ba73afc00860c19cfa4
Alexander Len <lenalexm@gmail.com>
2018-10-08 16:42:30 -0700

    Initial Commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,12 @@ public class PlayerScript : MonoBehaviour {
 
   [SerializeField]
 	private float movespeed;
+  [SerializeField]
+  private GameObject nearestInteractable, cursor;
+  [SerializeField]
+  private float range, cursorHeight;
+  [SerializeField]
+  private bool interacting;
   private Animator anim;
   private Rigidbody rb;
   private bool moving;
@@ -29,6 +35,7 @@ public class PlayerScript : MonoBehaviour {
       lastx = xaxis;
       anim.SetFloat("LastZ", zaxis);
       lastz = zaxis;
+      SearchInteractables();
     }
 
     if(lastx == -1){
@@ -40,4 +47,38 @@ public class PlayerScript : MonoBehaviour {
     rb.AddForce(movespeed * Time.deltaTime * zaxis * transform.forward, ForceMode.VelocityChange);
     rb.AddForce(movespeed * Time.deltaTime * xaxis * transform.right, ForceMode.VelocityChange);
 	}
+
+  void SearchInteractables(){
+    var collidersInRange = Physics.OverlapSphere(transform.position, range);
+    if(!interacting || collidersInRange.Length == 0){
+      cursor.SetActive(false);
+      Debug.Log("!interacting || collidersInRange.Length == 0");
+      return;
+    }
+    float distance = range;
+    Collider closestCollider = null;
+    Vector3 position = transform.position;
+    foreach (Collider currentCollider in collidersInRange){
+      if (currentCollider.tag == "Interactable"){
+        Vector3 diff = currentCollider.transform.position - position;
+        float curDistance = diff.sqrMagnitude;
+        if (curDistance < distance){
+          closestCollider = currentCollider;
+          distance = curDistance;
+        }
+      }
+    }
+    if(closestCollider == null){
+      cursor.SetActive(false);
+      nearestInteractable = null;
+      Debug.Log("closestCollider == null");
+      return;
+    }
+    if(closestCollider.gameObject != nearestInteractable){
+      nearestInteractable = closestCollider.gameObject;
+      cursor.transform.position = new Vector3(nearestInteractable.transform.position.x, nearestInteractable.transform.position.y + cursorHeight, nearestInteractable.transform.position.z);
+      cursor.SetActive(true);
+      Debug.Log("closestCollider.gameObject != nearestInteractable", nearestInteractable);
+    }
+  }
 }
