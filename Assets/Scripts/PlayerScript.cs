8e3a625d9814315c492209b3142f101b767f9fe1
Alexander Len <lenalexm@gmail.com>
2018-10-10 22:02:40 -0700

    Combined Interactivity with Shaders
    Combined Interactivity with Shaders manually. Also changed projectile code to have uniform strength.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,9 +8,9 @@ public class PlayerScript : MonoBehaviour {
   [SerializeField]
 	private float movespeed;
   [SerializeField]
-  private GameObject nearestInteractable, cursor;
+  private GameObject nearestInteractable, cursor, cube;
   [SerializeField]
-  private float range, cursorHeight;
+  private float range, cursorHeight, bowPower;
   [SerializeField] 
   private bool interacting, controlling, inMenu;
   [SerializeField] 
@@ -19,9 +19,13 @@ public class PlayerScript : MonoBehaviour {
   private Rigidbody rb;
   private bool moving;
   private float xaxis, zaxis, lastx, lastz;
+  private Camera mainCamera;
+
+
 	void Start () {
 		anim = GetComponent<Animator>();
     rb = GetComponent<Rigidbody>();
+    mainCamera = FindObjectOfType<Camera>();
     SearchInteractables();
 	}
 	
@@ -36,7 +40,32 @@ public class PlayerScript : MonoBehaviour {
     if(!interacting && Input.GetButtonDown("Fire1") && nearestInteractable != null){
       Interact();
     }
+    if(Input.GetButtonDown("Fire1") && Input.GetButton("Fire2") && controlling){
+      //On Mouse Left Button Press & Mouse Right is held down
+      Shoot();
+    }
 	}
+
+  void Shoot(){
+    GameObject projectile = Instantiate(cube) as GameObject;
+    projectile.transform.position = new Vector3(rb.transform.position.x, 1, rb.transform.position.z);
+
+    Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+    Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+    float enter = 0.0f;
+    if(groundPlane.Raycast(cameraRay, out enter)){
+      Vector3 hitPoint = cameraRay.GetPoint(enter);
+      Vector3 direction = hitPoint - transform.position;
+
+      //Some math to make magnitude of velocity vector uniform.
+      float sqrLen = direction.sqrMagnitude;
+      float distanceConstant = Mathf.Sqrt(Mathf.Pow(bowPower, 2)/sqrLen);
+
+      Physics.IgnoreCollision(projectile.GetComponent<Collider>(), GetComponent<Collider>());
+      Rigidbody rib = projectile.GetComponent<Rigidbody>();
+      rib.velocity = new Vector3(direction.x*distanceConstant, transform.position.y + 5, direction.z*distanceConstant);
+    }
+  }
 
   void Move(){
       xaxis = Input.GetAxisRaw("Horizontal");
