7b24057f1c64d03e1ebd31cee9a8be80cb3b88eb
Alexander Len <lenalexm@gmail.com>
2018-10-29 15:49:16 -0700

    Added Knockback, Monster State, and Monster Vision - Mebin
    Originally by Mebin from 'monster' branch. Manually comitted because of merge conflict.

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,20 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster : MonoBehaviour {
     public int health = 10;
+    private Rigidbody rb;
+    public GameObject player;
+    public float stun_time = 4.0f;
+    public float total_stun_time = 0;
+    private SphereCollider vision_circle;
+    private BoxCollider vision_square;
+    public enum ENEMY_STATES
+    {
+        IDLE = 0, 
+        ATTACKING = 1,
+        STUNNED = 2,
+        CHASING = 3
+    }
+    public ENEMY_STATES state = ENEMY_STATES.IDLE;
+    NavMeshAgent agent;
+    public void Start()
+    {
+        vision_circle = GetComponent<SphereCollider>();
+        vision_square = GetComponent<BoxCollider>();
+        agent = GetComponent<NavMeshAgent>();
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if(state != ENEMY_STATES.STUNNED) state = ENEMY_STATES.CHASING;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            state = ENEMY_STATES.IDLE;
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "projectile")
-        {
-            collision.gameObject.tag = "Untagged";
+        if (collision.gameObject.tag == "projectile") {
+            state = ENEMY_STATES.STUNNED;
             health--;
-            //Destroy(collision.gameObject);
+            rb.AddForce(collision.gameObject.transform.forward);
+            Destroy(collision.gameObject);
         }
-        if(health <= 0)
+        if(health <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(state == ENEMY_STATES.IDLE)
         {
-            Destroy(transform.gameObject);
+           
         }
+        else if (state == ENEMY_STATES.CHASING)
+        {
+            this.gameObject.transform.LookAt(player.gameObject.transform);
+            agent.destination = player.gameObject.transform.position;
+        }
+        else if(state == ENEMY_STATES.STUNNED)
+        {
+            total_stun_time += Time.deltaTime;
+            if (total_stun_time >= stun_time)
+            {
+                total_stun_time = 0;
+                state = ENEMY_STATES.CHASING;
+            }
+        }
+       
     }
 }
---
+++ Assets/Scripts/MonsterOld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterOld : MonoBehaviour {
+    public int health = 10;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "projectile")
+        {
+            collision.gameObject.tag = "Untagged";
+            health--;
+            //Destroy(collision.gameObject);
+        }
+        if(health <= 0)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
