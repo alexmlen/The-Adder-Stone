799dd145e0e3975eeb0dd91e2048b1ccca154c30
Alexander Len <lenalexm@gmail.com>
2018-10-31 22:56:29 -0700

    More Unfinished Rigging

--- Assets/Plugins/Anima2D/Scripts/Editor/HandlesExtra.cs
+++ Assets/Plugins/Anima2D/Scripts/Editor/HandlesExtra.cs
@@ -32,23 +32,23 @@ namespace Anima2D
 
 				dotCyan.fixedWidth = 8f;
 				dotCyan.fixedHeight = 8f;
-				dotCyan.normal.background = EditorGUIUtility.LoadRequired("Anima2D/dotCyan.png") as Texture2D;
+				dotCyan.normal.background = EditorGUIUtility.LoadRequired("Assets/Plugins/Anima2D/dotCyan.png") as Texture2D;
 
 				dotYellow.fixedWidth = 8f;
 				dotYellow.fixedHeight = 8f;
-				dotYellow.normal.background = EditorGUIUtility.LoadRequired("Anima2D/dotYellow.png") as Texture2D;
+				dotYellow.normal.background = EditorGUIUtility.LoadRequired("Assets/Plugins/Anima2D/dotYellow.png") as Texture2D;
 
 				dotRed.fixedWidth = 8f;
 				dotRed.fixedHeight = 8f;
-				dotRed.normal.background = EditorGUIUtility.LoadRequired("Anima2D/dotRed.png") as Texture2D;
+				dotRed.normal.background = EditorGUIUtility.LoadRequired("Assets/Plugins/Anima2D/dotRed.png") as Texture2D;
 
 				dotBlackBig.fixedWidth = 23.5f;
 				dotBlackBig.fixedHeight = 23.5f;
-				dotBlackBig.normal.background = EditorGUIUtility.LoadRequired("Anima2D/dotBlackBig.png") as Texture2D;
+				dotBlackBig.normal.background = EditorGUIUtility.LoadRequired("Assets/Plugins/Anima2D/dotBlackBig.png") as Texture2D;
 
 				dotYellowBig.fixedWidth = 23.5f;
 				dotYellowBig.fixedHeight = 23.5f;
-				dotYellowBig.normal.background = EditorGUIUtility.LoadRequired("Anima2D/dotYellowBig.png") as Texture2D;
+				dotYellowBig.normal.background = EditorGUIUtility.LoadRequired("Assets/Plugins/Anima2D/dotYellowBig.png") as Texture2D;
 			}
 		}
 
